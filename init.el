055d97c08ecee6f159c54768e6152dfc1692be4a
Johan S. R. Nielsen <atuin@atuin.dk>
2013-01-28 16:35:54 +0100

    Overhaul of some Evil rebindings, including added new section for Lisp utilities with code from Cofi. Added smart window bindings, and added Evil window movement to Emacs bindings.  Added surround.vim functionality.

--- init.el
+++ init.el
@@ -17,6 +17,56 @@
 (add-to-list 'auto-mode-alist '("\\.scene\\'" . xml-mode))
 (add-to-list 'auto-mode-alist '("\\.sage\\'" . python-mode))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;       ELISP UTILS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun def-assoc (key alist default)
+  "Return cdr of `KEY' in `ALIST' or `DEFAULT' if key is no car in alist."
+  (let ((match (assoc key alist)))
+    (if match
+        (cdr match)
+      default)))  
+
+(defun take (n lst)
+  "Return atmost the first `N' items of `LST'."
+  (let (acc '())
+    (while (and lst (> n 0))
+      (decf n)
+      (push (car lst) acc)
+      (setq lst (cdr lst)))
+    (nreverse acc)))
+
+(defun group (lst n)
+  "Group `LST' into portions of `N'."
+  (let (groups)
+    (while lst
+      (push (take n lst) groups)
+      (setq lst (nthcdr n lst)))
+    (nreverse groups)))
+
+(defun pour-mappings-to (map mappings)
+  "Calls `define-key' with `map' on every key-fun pair in `MAPPINGS'.
+`MAPPINGS' is a list of string-fun pairs, with a define-key-understandable string and a interactive-fun."
+  (dolist (mapping (group mappings 2))
+    (define-key map (car mapping) (cadr mapping)))
+  map)
+
+(defun fill-keymap (keymap &rest mappings)
+  "Fill `KEYMAP' with `MAPPINGS'.
+See `pour-mappings-to'."
+  (pour-mappings-to keymap mappings))
+
+(defun fill-keymaps (keymaps &rest mappings)
+  "Fill `KEYMAPS' with `MAPPINGS'.
+See `pour-mappings-to'."
+  (dolist (keymap keymaps keymaps)
+    (let ((map (if (symbolp keymap)
+                   (symbol-value keymap)
+                 keymap)))
+      (pour-mappings-to map mappings))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;       GLOBALLY DEFINED CUSTOM FUNCTIONS AND KEYS
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -33,6 +83,9 @@
 (global-set-key [(f5)] 'orgtbl-mode)
 (global-set-key [(shift f5)] 'orgtbl-insert-radio-table)
 (global-set-key "\M-?" 'hippie-expand)
+;; Other window control
+(global-set-key (kbd "C-M-b") 'scroll-other-window-down)
+(global-set-key (kbd "C-M-d") 'scroll-other-window)
 
 (defun kill-line-backwards ()
   "Kill the current line backwards from the current column.
@@ -104,9 +157,10 @@ If point was already at that position, move point to beginning of line."
 ;;       EVIL
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'evil)
-(setq evil-find-skip-newlines t)
-(setq evil-ex-search-highlight-all nil)
-(setq evil-want-fine-undo t)
+(setq evil-find-skip-newlines t
+      evil-move-cursor-back nil
+      evil-ex-search-highlight-all nil
+      evil-want-fine-undo t)
 (setq evil-normal-state-tag (propertize "N" 'face '((:background "green" :foreground "black")))
       evil-emacs-state-tag (propertize "E" 'face '((:background "orange" :foreground "black")))
       evil-insert-state-tag (propertize "I" 'face '((:background "red")))
@@ -114,44 +168,47 @@ If point was already at that position, move point to beginning of line."
       evil-visual-state-tag (propertize "V" 'face '((:background "grey80" :foreground "black")))
       evil-operator-state-tag (propertize "O" 'face '((:background "purple"))))
 ;; Escape quits anything
-(define-key evil-normal-state-map [escape] 'keyboard-quit)
-(define-key evil-visual-state-map [escape] 'keyboard-quit)
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-;; TAB is overtaken by Emacs (which is ok), so map jump-forward to C-Esc
-(define-key evil-normal-state-map (kbd "C-<escape>") 'evil-jump-forward)
-(define-key evil-visual-state-map (kbd "C-<escape>") 'evil-jump-forward)
-(define-key evil-insert-state-map (kbd "C-<escape>") 'evil-jump-forward)
-;; Control-e and Control-a works everywhere
-(define-key evil-normal-state-map (kbd "S-a") '(lambda () (interactive)
-                                                 (end-of-visual-line) (evil-insert-state)))
-(define-key evil-normal-state-map (kbd "C-e") 'end-of-visual-line)
-(define-key evil-insert-state-map (kbd "C-e") 'end-of-visual-line)
-(define-key evil-motion-state-map (kbd "C-e") 'end-of-visual-line)
-(define-key evil-normal-state-map (kbd "C-a") 'beginning-of-visual-line-smart)
-(define-key evil-insert-state-map (kbd "C-a") 'beginning-of-visual-line-smart)
-(define-key evil-motion-state-map (kbd "C-a") 'beginning-of-visual-line-smart)
-;; Search using Emacs' isearch but using Vim keybindings
-(define-key evil-normal-state-map "/" 'isearch-forward)
-(define-key evil-normal-state-map "?" 'isearch-backward)
-(define-key evil-normal-state-map "n" 'isearch-repeat-forward)
-(define-key evil-normal-state-map "N" 'isearch-repeat-backward)
-;; Move using visual lines
-(define-key evil-motion-state-map "j" #'evil-next-visual-line)
-(define-key evil-motion-state-map "k" #'evil-previous-visual-line)
-(define-key evil-motion-state-map "$" #'evil-end-of-visual-line)
-(define-key evil-motion-state-map "^" #'evil-first-non-blank-of-visual-line)
-(define-key evil-motion-state-map "0" #'evil-beginning-of-visual-line)
-;; Tab in normal mode works as tab in Emacs
-(define-key evil-normal-state-map (kbd "TAB") 'indent-for-tab-command)
-;; Provide yank at insert time
-(define-key evil-insert-state-map (kbd "C-y") 'yank)
-;; Provide a visual-time shorcut to commenting
-(define-key evil-visual-state-map "z" 'comment-region)
-(define-key evil-visual-state-map "Z" 'uncomment-region)
+(fill-keymaps (list evil-normal-state-map
+                   evil-visual-state-map)
+              [escape] 'keyboard-quit)
+(fill-keymaps (list minibuffer-local-map
+                   minibuffer-local-ns-map
+                   minibuffer-local-completion-map
+                   minibuffer-local-must-match-map)
+              [escape] 'minibuffer-keyboard-quit)
+;; Key-bindings in all modes
+(fill-keymaps (list evil-normal-state-map
+                    evil-visual-state-map
+                    evil-insert-state-map)
+              ;; TAB is overtaken by Emacs (which is ok), so map jump-forward to C-Esc
+              (kbd "C-<escape>") 'evil-jump-forward
+              (kbd "C-e")        'end-of-visual-line
+              (kbd "C-a")        'beginning-of-visual-line-smart
+              (kbd "C-b")        'evil-scroll-up)
+;; Key-bindings in normal mode
+(fill-keymap evil-normal-state-map
+             (kbd "S-a") '(lambda () (interactive) (end-of-visual-line) (evil-insert-state))
+             ;; Search using Emacs' isearch but using Vim keybindings
+             "/" 'isearch-forward
+             "?" 'isearch-backward
+             "n" 'isearch-repeat-forward
+             "N" 'isearch-repeat-backward
+             ;; Tab in normal mode works as tab in Emacs
+             (kbd "TAB") 'indent-for-tab-command)
+;; Key-bindings in insert mode
+(fill-keymap evil-insert-state-map
+             (kbd "C-y") 'yank)
+;; Key-bindings in visual mode
+(fill-keymap evil-visual-state-map
+             ;; Provide a visual-time shorcut to commenting
+             "z" 'comment-region
+             "Z" 'uncomment-region)
+;; Key-bindings for movement
+(fill-keymap evil-motion-state-map
+             "j" 'evil-next-visual-line
+             "k" 'evil-previous-visual-line
+             "$" 'evil-end-of-visual-line
+             "^" 'evil-first-non-blank-of-visual-line)
 
 ; Disable Evil in certain modes
 (loop for (mode . state) in '((eassist-mode . emacs)
@@ -161,14 +218,27 @@ If point was already at that position, move point to beginning of line."
 
 (evil-mode 1)
 
+; In some modes, the special Enter is most I need, so map this to C-<enter>
+(loop for (mode . fun) in '((man-mode . man-follow))
+      do (evil-declare-key normal mode "C-<enter>" 'fun))
+
+;; Emulate surround.vim
+;; Usage description really quick:
+;; type c s <delimiter> <replacement> for replacing the nearest <delimiter> pair
+;; with appropriately chosen <replacement> pair. Use left delimiter to get space
+;; on insider, and right delimiter otherwise
+;; type d s <delimiter> to remove delimiter pair
+;; In visual mode, type s <new delim> to insert delimiter (same rules w. spaces)
+;;                 type S <new delim> to insert also newlines on inside
+(require 'surround)
+(surround-mode 1)
+;; Some extras for certain modes
+(add-hook 'LaTeX-mode-hook (lambda ()
+                             (push '(?~ . ("\\texttt{" . "}")) surround-pairs-alist)
+                             (push '(?/ . ("\\emph{"   . "}")) surround-pairs-alist)
+                             (push '(?* . ("\\textbf{" . "}")) surround-pairs-alist)))
+
 ;; make cursor look like Vim when in Vim normal moe
-;;TODO: Move the following function to some utils
-(defun def-assoc (key alist default)
-  "Return cdr of `KEY' in `ALIST' or `DEFAULT' if key is no car in alist."
-  (let ((match (assoc key alist)))
-    (if match
-        (cdr match)
-      default)))  
 (defun cofi/evil-cursor ()
   "Change cursor color according to evil-state."
   (let ((color-default "OliveDrab4")
@@ -182,13 +252,12 @@ If point was already at that position, move point to beginning of line."
     (set-cursor-color (def-assoc evil-state cursors color-default))))
 (setq evil-default-cursor #'cofi/evil-cursor)
 
-;; To reinstate Evil in modes where Emacs has simple keys
-(defun reinstate-vim-motion mode-map
-  (evil-define-key 'normal dired-mode-map
-    "h" 'evil-backward-char
-    "j" 'evil-next-line
-    "k" 'evil-previous-line
-    "l" 'evil-forward-char))
+(eval-after-load 'man
+  '(progn
+     ;; use Evil but add some keys
+     (evil-make-overriding-map evil-normal-state-map 'normal t)
+     (evil-define-key 'normal
+       "C-<enter>" 'man-follow nil))) ;the nil is needed for only one define; it's a bug in Evil
 
 (eval-after-load 'dired
   '(progn
@@ -200,44 +269,25 @@ If point was already at that position, move point to beginning of line."
        "r" 'dired-do-redisplay))) ; was "l"
 
 ;; windowing
-;; (fill-keymap evil-window-map
-;;     "C-g" nil
-;;     ;; Splitting
-;;     "\\" 'split-window-vertically
-;;     "|" 'split-window-horizontally
+(fill-keymap evil-window-map
+             (kbd "C-g") nil
+             ;; Splitting
+             "\\" 'split-window-vertically
+             "|"  'split-window-horizontally
+             ;; Deleting
+             "D" 'delete-window
+             (kbd "C-d") 'delete-window
+             "1" 'delete-other-windows
+             ;; Sizing
+             (kbd "RET") 'enlarge-window)
 
-;;     ;; Deleting
-;;     "D" 'delete-window
-;;     "C-d" 'delete-window
-;;     "1" 'delete-other-windows
-
-;;     ;; Sizing
-;;     "RET" 'enlarge-window
-;;     "-" 'shrink-window-horizontally
-;;     "+" 'enlarge-window-horizontally
-
-;;     ;; Moving
-;;     ;"h" 'evil-window-left
-;;     ;"j" 'evil-window-down
-;;     ;"k" 'evil-window-up
-;;     ;"l" 'evil-window-right
-
-;;     ;; Swapping
-;;     "M-h" 'swap-with-left
-;;     "M-j" 'swap-with-down
-;;     "M-k" 'swap-with-up
-;;     "M-l" 'swap-with-right
-;;     "S-<left>" 'swap-with-left
-;;     "S-<down>" 'swap-with-down
-;;     "S-<up>" 'swap-with-up
-;;     "S-<right>" 'swap-with-right
-;;     "SPC" 'swap-window
-
-;;     ;; winner-mode
-;;     "u" 'winner-undo
-;;     "C-r" 'winner-redo
-;;     ;; shadow rotating in evil-window-map
-;;     "C-R" 'winner-redo)
+;; Put selected Evil Keys in Emacs mode
+(fill-keymap evil-emacs-state-map
+             (kbd "C-w h") 'evil-window-left
+             (kbd "C-w j") 'evil-window-down
+             (kbd "C-w k") 'evil-window-up
+             (kbd "C-w l") 'evil-window-right
+             (kbd "C-w C-w") 'evil-window-prev)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
