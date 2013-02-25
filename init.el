859414a3adfed8f5352a88e1dcca6fff9e27ea11
Johan S. R. Nielsen <atuin@atuin.dk>
2013-02-25 11:35:34 +0100

    More mu4e setup. Removed recurring items from showing each time in agenda view

--- init.el
+++ init.el
@@ -310,6 +310,8 @@ sometimes if more than one Emacs has this set"
   (interactive)
   ;; Set files which contains agenda files to all .org files in specified dir
   (setq org-agenda-files (directory-files "~/orgs" t ".org$" t))
+  ;; Various agenda setup
+  (setq org-agenda-repeating-timestamp-show-all nil) ; don't show repititions in agenda
   ;; Reminder support for Org
   (defun jsrn-org-agenda-to-appt ()
     "Erase all reminders and rebuilt reminders for today from the agenda"
@@ -567,6 +569,25 @@ sometimes if more than one Emacs has this set"
 (defun jsrn-mu4e-setup ()
   (add-to-list 'load-path"/usr/local/share/emacs/site-lisp/mu4e")
   (require 'mu4e)
+  ;; Handling html messages
+  (setq mu4e-html2text-command "html2text -utf8 -width 72")
+  (defun mu4e-view-in-browser ()
+    "View the body of the message in a web browser."
+    (interactive)
+    (let (msg txt html)
+      (setq msg (mu4e-message-at-point t))
+      (setq txt (plist-get msg :body-txt))
+      (setq html (plist-get msg :body-html))
+      (with-temp-buffer
+        (when (> (length html) 0)
+          (insert html))
+        (when (> (length txt) 0)
+          (insert txt))
+        (browse-url-of-buffer)))) 
+  ;; Call view-in-browser when pressing "ab" (the leading b in str below is key)
+  (add-to-list 'mu4e-view-actions
+               '("bView in browser" . mu4e-action-view-in-browser) t)
+  ;; Set up the mailboxes for refiling, sent etc.
   (defun jsrn-mu4e-mailbox (msg)
     (let ((maildir (mu4e-message-part-field msg :maildir)))
       (if (eq nil (string-match "^/\\([^/]*\\)/" maildir))
@@ -574,30 +595,36 @@ sometimes if more than one Emacs has this set"
         (match-string 1 maildir)
         )))
   (setq mu4e-maildir "~/mail"
-        ;;TODO: Make these functions to go into specific account archives
-        mu4e-sent-folder "/sent"
+        mu4e-sent-folder (lambda (msg)
+                           (if (eq msg nil)
+                               "/atuin/INBOX.Sent"
+                             (let ((mailbox (jsrn-mu4e-mailbox msg)))
+                               (cond ((string-equal mailbox "atuin") "/atuin/INBOX.Sent")
+                                     ((string-equal mailbox "jsrn") "/atuin/INBOX.Sent")
+                                     ((string-equal mailbox "dtu")   "/dtu/Sent")))))
         mu4e-drafts-folder "/drafts"
         mu4e-trash-folder "/trash"
         mu4e-refile-folder (lambda (msg)
                              (let ((mailbox (jsrn-mu4e-mailbox msg)))
                                (cond ((string-equal mailbox "atuin") "/atuin/INBOX.Archives.2013")
+                                     ((string-equal mailbox "jsrn") "/atuin/INBOX.Archives.2013")
                                      ((string-equal mailbox "dtu")   "/dtu/Archives.2013"))))
         )
+  (setq mu4e-attachment-dir "~/downloads")
+  ;; Check mail using offlineimap every 5 min
   (setq mu4e-get-mail-command "offlineimap"
         mu4e-update-interval 300
         mail-user-agent 'mu4e-user-agent)
-  (setq mu4e-attachment-dir "~/downloads")
+  ;; Messages should be replied from the recipient if it was one of the
+  ;; registered email addresses
   (setq mu4e-my-email-addresses '("atuin@atuin.dk"
                                   "jsrn@atuin.dk"
                                   "johan@atuin.dk"
+                                  "jsrn@jsrn.dk"
                                   "webmaster@atuin.dk"
                                   "spammy@atuin.dk"
                                   "j.s.r.nielsen@mat.dtu.dk"
                                   "jsrn@dtu.dk"))
-  (setq mu4e-confirm-quit nil)
-  (add-hook 'mu4e-compose-mode-hook (defun jsrn-mu4e-compose-setup ()
-                                      (flyspell-mode t)))
-  ;; Set up that messages are replied from the recipient
   (add-hook 'mu4e-compose-pre-hook
             (defun my-set-from-address ()
               "Set the From address based on the To address of the original."
@@ -608,6 +635,9 @@ sometimes if more than one Emacs has this set"
                         (if (member toaddr mu4e-my-email-addresses)
                             toaddr
                           jsrn-user-mail-address))))))
+  (setq mu4e-confirm-quit nil)
+  (add-hook 'mu4e-compose-mode-hook (defun jsrn-mu4e-compose-setup ()
+                                      (flyspell-mode t)))
   (define-key mu4e-headers-mode-map (kbd "<backspace>")
     ;; backspace should clear mark like in dired
     (lambda () (interactive)
