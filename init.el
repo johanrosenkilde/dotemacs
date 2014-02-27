9435803e5353ad7d7a389893dbd81467df040789
Johan S. R. Nielsen <atuin@atuin.dk>
2014-02-27 11:14:04 +0100

    Monky: primitive Magit for Mercurial

--- init.el
+++ init.el
@@ -1359,11 +1359,32 @@ complete card names"
 	     )
 (defun jsrn-magit-mode-hook ()
   (interactive)
+  (visual-line-mode)
   (fill-keymap evil-motion-state-local-map
                (kbd (concat "C-" evil-down-key)) 'evil-next-line
                (kbd (concat "C-" evil-up-key))   'evil-previous-line
                ))
 (add-hook 'magit-mode-hook 'jsrn-magit-mode-hook)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;       MONKY
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'monky)
+(evil-set-initial-state 'monky-mode 'normal)
+(fill-keymap monky-mode-map
+	     (kbd "<return>") (lambda () (interactive) (monky-visit-item t))
+	     (kbd "S-SPC")    'monky-show-item-or-scroll-down
+             evil-down-key 'monky-goto-next-section
+             evil-up-key   'monky-goto-previous-section
+	     )
+(defun jsrn-monky-mode-hook ()
+  (interactive)
+  (fill-keymap evil-motion-state-local-map
+               (kbd (concat "C-" evil-down-key)) 'evil-next-line
+               (kbd (concat "C-" evil-up-key))   'evil-previous-line
+               ))
+(add-hook 'monky-mode-hook 'jsrn-monky-mode-hook)
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
