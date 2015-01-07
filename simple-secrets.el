58f056d3a23e4b17d60bf74618128bc5c9983ecf
Johan S. R. Nielsen <jsrn@jsrn.dk>
2015-01-07 16:35:27 +0100

    secrets: temporary workaround for bug in Emacs 25 gpg.

--- simple-secrets.el
+++ simple-secrets.el
@@ -36,6 +36,53 @@
 ;;; be specified with `secret-generate-password-command'. By default
 ;;; it uses pwgen.
 
+
+
+;; TEMPORARY: To fix an upstream bug introduced in Emacs 25
+;; https://bbs.archlinux.org/viewtopic.php?id=190497
+(defun epg--list-keys-1 (context name mode)
+  (let ((args (append (if (epg-context-home-directory context)
+			  (list "--homedir"
+				(epg-context-home-directory context)))
+		      '("--with-colons" "--no-greeting" "--batch"
+			"--with-fingerprint" "--with-fingerprint")
+		      (unless (eq (epg-context-protocol context) 'CMS)
+			'("--fixed-list-mode"))))
+	(list-keys-option (if (memq mode '(t secret))
+			      "--list-secret-keys"
+			    (if (memq mode '(nil public))
+				"--list-keys"
+			      "--list-sigs")))
+	(coding-system-for-read 'binary)
+	keys string field index)
+    (if name
+	(progn
+	  (unless (listp name)
+	    (setq name (list name)))
+	  (while name
+	    (setq args (append args (list list-keys-option (car name)))
+		  name (cdr name))))
+      (setq args (append args (list list-keys-option))))
+    (with-temp-buffer
+      (apply #'call-process
+	     (epg-context-program context)
+	     nil (list t nil) nil args)
+      (goto-char (point-min))
+      (while (re-search-forward "^[a-z][a-z][a-z]:.*" nil t)
+	(setq keys (cons (make-vector 15 nil) keys)
+	      string (match-string 0)
+	      index 0
+	      field 0)
+	(while (and (< field (length (car keys)))
+		    (eq index
+			(string-match "\\([^:]+\\)?:" string index)))
+	  (setq index (match-end 0))
+	  (aset (car keys) field (match-string 1 string))
+	  (setq field (1+ field))))
+      (nreverse keys))))
+
+
+
 (defgroup simple-secrets nil
   "Group for the `simple-secrets' package"
   )
@@ -68,7 +115,6 @@ the passwords in any Emacs variables; only the keys."
                      (buffer-substring start (point)))
         (forward-line)))
   ))
-
 (defun secret-lookup (key)
   "Return the password for the given key."
   (interactive
