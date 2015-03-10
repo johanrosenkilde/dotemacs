5dcacaa5f19033598978da902d966910753875a1
Johan S. R. Nielsen <jsrn@jsrn.dk>
2015-03-10 17:26:08 +0100

    Haskell setup

--- init.el
+++ init.el
@@ -1048,8 +1048,12 @@ the optional values set"
 ;;       HASKELL
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun jsrn-haskell-mode-hook ()
-  (turn-on-haskell-doc-mode "haskell-doc" nil t)
+  ;; (turn-on-haskell-doc-mode "haskell-doc" nil t)
   (turn-on-haskell-indentation)
+  (setq haskell-process-type 'cabal-repl)
+  (interactive-haskell-mode)
+  (evil-set-initial-state 'haskell-interactive-mode 'emacs)
+  (evil-set-initial-state 'haskell-error-mode 'emacs)
   )
 (add-hook 'haskell-mode-hook 'jsrn-haskell-mode-hook)
 
