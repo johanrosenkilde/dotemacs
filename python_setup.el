18a7b3e9dfe02e3b68fcd7586c56023e1b0e6688
Johan Rosenkilde <jsrn@jsrn.dk>
2017-12-13 13:58:52 +0100

    Python: Don't use python-mode since it is very buggy

--- python_setup.el
+++ python_setup.el
@@ -1,79 +1,79 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Setup Emacs-wide loads, vars etc.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'python-mode)
+(require 'python)
 
 ; use IPython
-(setq-default py-shell-name "ipython2")
-(setq-default py-which-bufname "IPython")
-                                ; use the wx backend, for both mayavi and matplotlib
-(setq py-python-command-args
-      '("--gui=wx" "--pylab=wx" "Linux"))
-(setq py-force-py-shell-name-p t)
+;; (setq-default py-shell-name "ipython2")
+;; (setq-default py-which-bufname "IPython")
+;;                                 ; use the wx backend, for both mayavi and matplotlib
+;; (setq py-python-command-args
+;;       '("--gui=wx" "--pylab=wx" "Linux"))
+;; (setq py-force-py-shell-name-p t)
 
-                                ; switch to the interpreter after executing code
-(setq py-shell-switch-buffers-on-execute-p t)
-(setq py-switch-buffers-on-execute-p t)
-                                ; don't split windows
-(setq py-split-windows-on-execute-p nil)
-                                ; try to automagically figure out indentation
-(setq py-smart-indentation t)
+;;                                 ; switch to the interpreter after executing code
+;; (setq py-shell-switch-buffers-on-execute-p t)
+;; (setq py-switch-buffers-on-execute-p t)
+;;                                 ; don't split windows
+;; (setq py-split-windows-on-execute-p nil)
+;;                                 ; try to automagically figure out indentation
+;; (setq py-smart-indentation t)
 
-(setq rope-active nil)
-(defun rope-activate ()
-  (interactive)
-  (setq rope-active t)
+;; (setq rope-active nil)
+;; (defun rope-activate ()
+;;   (interactive)
+;;   (setq rope-active t)
 
-  (add-to-list 'load-path "/home/jsrn/local/Pymacs/")
-  (autoload 'pymacs-apply "pymacs")
-  (autoload 'pymacs-call "pymacs")
-  (autoload 'pymacs-eval "pymacs" nil t)
-  (autoload 'pymacs-exec "pymacs" nil t)
-  (autoload 'pymacs-load "pymacs" nil t)
-  (autoload 'pymacs-autoload "pymacs")
-  (setq pymacs-python-command "python2")
-  ;; The following is to add custom Pymacs code which is run by Python
-  ;;(eval-after-load "pymacs"
-  ;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
+;;   (add-to-list 'load-path "/home/jsrn/local/Pymacs/")
+;;   (autoload 'pymacs-apply "pymacs")
+;;   (autoload 'pymacs-call "pymacs")
+;;   (autoload 'pymacs-eval "pymacs" nil t)
+;;   (autoload 'pymacs-exec "pymacs" nil t)
+;;   (autoload 'pymacs-load "pymacs" nil t)
+;;   (autoload 'pymacs-autoload "pymacs")
+;;   (setq pymacs-python-command "python2")
+;;   ;; The following is to add custom Pymacs code which is run by Python
+;;   ;;(eval-after-load "pymacs"
+;;   ;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
 
-  ;; Setup environment for when running Pymacs and python
-  (setq python-custom-libs (list "Pymacs" "rope" "ropemode" "ropemacs"))
-  (setq pythonpath "")
-  (cl-loop for lib in python-custom-libs
-           do (setq pythonpath (concat pythonpath (if (string-equal "" pythonpath)
-                                                      "" ":") "/home/jsrn/local/"
-                                                      lib "/build/lib/")))
-  (setenv "PYTHONPATH" pythonpath)
-  (setenv "PYMACS_PYTHON" "python2")
-  (setq pymacs-load-path '("/home/jsrn/local/rope/build/lib/rope"
-                           "/home/jsrn/local/ropemacs/build/lib/ropemacs"
-                           "/home/jsrn/local/ropemode/build/lib/ropemode"
-                           ))
-  (require 'pymacs)
+;;   ;; Setup environment for when running Pymacs and python
+;;   (setq python-custom-libs (list "Pymacs" "rope" "ropemode" "ropemacs"))
+;;   (setq pythonpath "")
+;;   (cl-loop for lib in python-custom-libs
+;;            do (setq pythonpath (concat pythonpath (if (string-equal "" pythonpath)
+;;                                                       "" ":") "/home/jsrn/local/"
+;;                                                       lib "/build/lib/")))
+;;   (setenv "PYTHONPATH" pythonpath)
+;;   (setenv "PYMACS_PYTHON" "python2")
+;;   (setq pymacs-load-path '("/home/jsrn/local/rope/build/lib/rope"
+;;                            "/home/jsrn/local/ropemacs/build/lib/ropemacs"
+;;                            "/home/jsrn/local/ropemode/build/lib/ropemode"
+;;                            ))
+;;   (require 'pymacs)
 
-  (global-set-key
-               (kbd "M-?")             'hippie-expand
-               )
-  (setq ropemacs-enable-autoimport t)
-  ;; (fill-keymap ropemacs-local-keymap
-  ;;              (kbd "C-c #") 'rope-goto-definition 
-  ;;              )
-  )
+;;   (global-set-key
+;;                (kbd "M-?")             'hippie-expand
+;;                )
+;;   (setq ropemacs-enable-autoimport t)
+;;   ;; (fill-keymap ropemacs-local-keymap
+;;   ;;              (kbd "C-c #") 'rope-goto-definition 
+;;   ;;              )
+;;   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Function for setting up each buffer
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun jsrn-python-mode-hook ()
   (interactive)
-  (add-to-list 'company-backends 'company-jedi)
-  (if rope-active
-      (progn
-        (pymacs-load "ropemacs" "rope-")
-        )
-    )
-  (fill-keymap python-mode-map
-               (kbd "C-<backspace>")     nil    ; disable stupid override
-               )
+  ;; (add-to-list 'company-backends 'company-jedi)
+  ;; (if rope-active
+  ;;     (progn
+  ;;       (pymacs-load "ropemacs" "rope-")
+  ;;       )
+  ;;   )
+  ;; (fill-keymap python-mode-map
+  ;;              (kbd "C-<backspace>")     nil    ; disable stupid override
+  ;;              )
   ;; (require 'pretty-lambdada) ;typeset word "lambda" as the symbol
   ;; (pretty-lambda-mode 1)
   )
