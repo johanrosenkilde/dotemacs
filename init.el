e511751e2903c1c4550f4ac0fe013c684fb74512
Johan Rosenkilde <jsrn@jsrn.dk>
2018-02-02 10:42:37 +0100

    Speed up startup snatched from Ambrevar

--- init.el
+++ init.el
@@ -1,3 +1,18 @@
+;;; Speed up init
+;;; Copied from Ambrevar: https://github.com/Ambrevar/dotfiles/blob/master/.emacs.d/init.el
+;;; Temporarily reduce garbage collection during startup. Inspect `gcs-done'.
+(defun reset-gc-cons-threshold ()
+  (setq gc-cons-threshold (car (get 'gc-cons-threshold 'standard-value))))
+(setq gc-cons-threshold (* 64 1024 1024))
+(add-hook 'after-init-hook 'reset-gc-cons-threshold)
+;;; Temporarily disable the file name handler.
+(setq default-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+(defun reset-file-name-handler-alist ()
+  (setq file-name-handler-alist default-file-name-handler-alist))
+(add-hook 'after-init-hook 'reset-file-name-handler-alist)
+
+
 ;; Keyboard layout to expect
 (setq workman t)
 
