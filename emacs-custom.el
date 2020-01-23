8c3d6ccdd06ef72d08b0a4a3b01220d5e6435064
Johan Rosenkilde <jsrn@jsrn.dk>
2020-01-23 09:39:11 +0100

    emacs-custom maintained package-selected-packages

---
+++ emacs-custom.el
@@ -0,0 +1,14 @@
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (imenu-anywhere zenburn-theme yaml-mode websocket web-mode use-package tuareg surround smex skewer-mode request psvn pretty-lambdada popwin php-mode paredit org offlineimap multi-term monky magit-popup magit lsp-ui ledger-mode ipython ido-ubiquitous highlight-parentheses help-mode+ help+ haskell-mode graphql goto-last-change ghub flycheck flx-isearch flx-ido evil-numbers evil dired+ diminish desktop csharp-mode counsel company-web company-quickhelp company-jedi company-bibtex company-auctex cm-mode cargo calfw-org calfw autopair auto-complete adoc-mode adaptive-wrap ace-jump-mode))))
