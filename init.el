6072c627871e43b8fbf84249c9cb00221ece5219
Johan Rosenkilde <jsrn@jsrn.dk>
2020-07-15 14:17:05 +0200

    cheat.sh keybinding. Disable printing pr default

--- init.el
+++ init.el
@@ -1313,25 +1313,33 @@ the optional values set"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;       PRINTING
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'printing)		; load printing package
-(setq jsrn-pr-printer-alist
-      '((et2-006 "lpr"     nil "et2-color-konica-006")   ;; Ulm University
-        ))
-(setq jsrn-current-printer 'et2-006)
-(defun jsrn-pr-set-printers ()
-  (interactive)
-  (setq pr-path-alist
-        '((unix      "." ghostview mpage PATH)
-          (ghostview "/usr/bin/gv")
-          (mpage     "/usr/bin/mpage")
-          ))
-  (setq pr-txt-printer-alist jsrn-pr-printer-alist)
-  (setq pr-ps-printer-alist jsrn-pr-printer-alist)
-  (setq pr-txt-name  jsrn-current-printer)
-  (setq pr-ps-name  jsrn-current-printer)
-  (pr-update-menus t)		; update now printer and utility menus
-)
-(jsrn-pr-set-printers)
+;; Printing only rarely used - no need to enable it
+;; (require 'printing)		; load printing package
+;; (setq jsrn-pr-printer-alist
+;;       '((et2-006 "lpr"     nil "et2-color-konica-006")   ;; Ulm University
+;;         ))
+;; (setq jsrn-current-printer 'et2-006)
+;; (defun jsrn-pr-set-printers ()
+;;   (interactive)
+;;   (setq pr-path-alist
+;;         '((unix      "." ghostview mpage PATH)
+;;           (ghostview "/usr/bin/gv")
+;;           (mpage     "/usr/bin/mpage")
+;;           ))
+;;   (setq pr-txt-printer-alist jsrn-pr-printer-alist)
+;;   (setq pr-ps-printer-alist jsrn-pr-printer-alist)
+;;   (setq pr-txt-name  jsrn-current-printer)
+;;   (setq pr-ps-name  jsrn-current-printer)
+;;   (pr-update-menus t)		; update now printer and utility menus
+;; )
+;; (jsrn-pr-set-printers)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;       CHEAT.SH
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'cheat-sh)
+(global-set-key (kbd "C-h q") 'cheat-sh)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
