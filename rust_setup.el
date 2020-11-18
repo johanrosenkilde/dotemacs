ec173e7a11ae16a1442280dc062edefb25c6f37b
Johan Rosenkilde <jsrn@jsrn.dk>
2020-11-18 14:26:25 +0100

    Rust some basic setup

--- rust_setup.el
+++ rust_setup.el
@@ -10,18 +10,26 @@
 
 (add-hook 'rust-mode-hook #'lsp)
 
+;; (add-hook 'rust-mode-hook #'racer-mode)
+;; (add-hook 'rust-mode-hook #'eldoc-mode)
+;; (add-hook 'rust-mode-hook #'company-mode)
+;; (setq company-tooltip-align-annotations t)
+
 (setq lsp-rust-show-warnings nil)
 
 (fill-keymap rust-mode-map
              [(f2)] 'compile
+             ;; (kbd "TAB") 'company-indent-or-complete-common
              )
 
 ;; ;; Add keybindings for interacting with Cargo
 ;; (use-package cargo
 ;;   :hook (rust-mode . cargo-minor-mode))
 
-;; (use-package flycheck-rust
-;;   :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+(use-package flycheck-rust
+  :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+(add-hook 'rust-mode-hook #'undo-tree-mode)
 
 ;; (rust-mode)
 
